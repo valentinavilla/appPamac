4096934ac004a0f3c40297db9d2ba82feba6c040
valentinavilla <68279203+valentinavilla@users.noreply.github.com>
2022-07-25 14:27:38 +0200

    navigazione tra views

--- types.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.css' {
-  import { CSSResultGroup } from 'lit';
-  const content: CSSResultGroup;
-  export default content;
-}
-
-declare module "*.jpg"
-declare module "*.png"
-declare module "*.gif"
